fe45c64f2cf24f40ee86d9fd8a7deae37b50892c
Arseno25 <utomnestagumara@gmail.com>
2025-11-19 13:57:25 +0700

    ♻️ (code-snippet-wrapper.blade.php): refactor conditional logic and include code snippet via dedicated template for better maintainability 📝 (code-snippet-wrapper.blade.php): prepare for consistent snippet rendering by initializing variables early

--- resources/views/code-snippet-wrapper.blade.php
+++ resources/views/code-snippet-wrapper.blade.php
@@ -1,14 +1,16 @@
 @php
     $record = $getRecord();
+    $snippet = null;
+    $extension = null;
+
+    if ($record && $record->file && $record->line && file_exists($record->file)) {
+        $snippet = $record->getSnippet();
+        $extension = $record->getFileExtension();
+    }
 @endphp
 
-@if($record && $record->file && $record->line && file_exists($record->file))
-    <div>
-        @livewire(\Arseno25\ExceptionLogger\Components\CodeSnippet::class, ['record' => $record], key('code-snippet-'.$record->id))
-    </div>
-@else
-    <div class="exception-logger-code-snippet__empty">
-        <p class="text-sm text-gray-500">Source code preview is not available. File may not exist or is not accessible.</p>
-    </div>
-@endif
+@include('exception-logger::code-snippet', [
+    'snippet' => $snippet,
+    'extension' => $extension,
+])
 
